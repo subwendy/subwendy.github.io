e3d268c757d84cbc25090abf7d8c441b99c3630c
subwendy <rueyaalp@outlook.de>
2021-06-20 17:42:07 +0200

    why are we here just to suffer

--- youtube.external.subtitle/src/youtube.external.subtitle.test.ts
+++
@@ -1,6 +0,0 @@
-import YoutubeExternalSubtitle from './youtube.external.subtitle';
-import Subtitle from './subtitle';
-
-test('the main script returns the Subtitle class', () => {
-  expect(YoutubeExternalSubtitle.Subtitle).toBe(Subtitle);
-});
